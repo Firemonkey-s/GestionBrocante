732e227606b300ff4d192a1a5b43a6869330f362
Firemonkey-s <77614509+Firemonkey-s@users.noreply.github.com>
2021-02-08 11:06:37 +0100

    Add files via upload

---
+++ Controller/Cartes.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Cartes
+    {
+        public function __construct()
+        {
+           // include("model/Role_manager.php");
+           // include("model/Role_class.php");
+        }
+        
+        // Gilles
+        public function EditerEmplacement()
+        {   
+            $RolesManager = new Role_manager();
+            $arrRoles = $RolesManager->findAll();
+            include("view/Header.php");
+            include("view/Localisation.php");  
+            include("View/Footer.php");
+        }
+
+       // Jean Philippes
+       public function EditerLocalisation()
+       {   
+         $RolesManager = new Role_manager();
+         $arrRoles = $RolesManager->findAll();
+         include("view/Header.php");
+         include("view/Emplacement.php");  
+         include("View/Footer.php");
+       }
+    }
