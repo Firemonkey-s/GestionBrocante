c05f647c91d4ef1096b787b08ce2a512868d721e
Firemonkey-s <77614509+Firemonkey-s@users.noreply.github.com>
2021-02-08 11:04:15 +0100

    Add files via upload

---
+++ View/Roles.php
@@ -0,0 +1,11 @@
+<h2>Liste des roles</h2>
+<?php 
+foreach ($arrRoles as $arrRole){
+                        $objRole = new role_class();
+                        $objRole->hydrate($arrRole);
+                  ?>
+                          <p>   Titre : <?php echo $objRole->GetTitre() ?><a href="">  Modifier</a></p>
+                        <?php } ?>
+
+
+
